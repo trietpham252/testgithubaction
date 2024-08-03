08de1e1df769d2368e71eb7c26e8921358776ffd
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2024-08-03 22:08:08 +0700

    czc

--- cypress/e2e/sample.cy.js
+++ cypress/e2e/sample.cy.js
@@ -1,7 +1,6 @@
 describe('Sample Test', () => {
-    it('should visit the app', () => {
-      cy.visit('/')
-      cy.contains('Welcome to Your App') // Đảm bảo rằng nội dung này khớp với ứng dụng của bạn
-    })
+  it('should visit the app', () => {
+    cy.visit('/')
+    cy.contains('Welcome to Your App') // Đảm bảo rằng nội dung này khớp với ứng dụng của bạn
   })
-  
+})
