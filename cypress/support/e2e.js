b395d633da0e4fccfc73dea95ede7a6b2690c660
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2024-08-03 21:55:37 +0700

    as

---
+++ cypress/support/e2e.js
@@ -0,0 +1,8 @@
+// This is the support file for Cypress end-to-end tests
+// You can include any global setup or configurations here
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
